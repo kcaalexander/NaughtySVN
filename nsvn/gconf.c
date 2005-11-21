bde20f18f128be908b1bea41b5738a15d939737a
Alexander Thomas <alexander@collab.net>
2005-11-21 13:59:47 +0000

    Package directory and GConf files added

---
+++ nsvn/gconf.c
@@ -0,0 +1,20 @@
+/*
+* ====================================================================
+* File will contain global gconf manipulation functions, which can be 
+* used in nsvn module.
+* Copyright (c) 2005, All rights reserved for Alexander Thomas (AT)
+*
+*
+* Leaving space here for Microsoft, to add license details.
+*
+*
+* ====================================================================
+*/
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gnome.h>
+
+#include "gconf.h"
---
+++ nsvn/gconf.h
@@ -0,0 +1,13 @@
+/*
+* ====================================================================
+* Header file for gconf manupulation.
+* Copyright (c) 2005 Alexander Thomas (AT) - NaughtySVN.
+*
+* 
+* Leaving space here for Microsoft, to add license details.
+*
+*
+* ====================================================================
+*/
+
+#include <gconf/gconf-client.h>
