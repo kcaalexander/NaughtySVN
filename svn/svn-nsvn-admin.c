67c97d7243fe5c2b350b1336f7330665cb7a6270
Alexander Thomas <alexander@collab.net>
2006-01-31 18:25:17 +0000

    Fixed alignment.

--- svn/svn-nsvn-admin.c
+++ svn/svn-nsvn-admin.c
@@ -35,6 +35,7 @@
 #include "svn-nsvn-private.h"
 
 #include <stdlib.h>
+
 int
 nsvn_admin_create_repos (void *n, int bdb_nosync,
                          int bdb_autorm,
@@ -42,56 +43,56 @@ nsvn_admin_create_repos (void *n, int bdb_nosync,
                          const char *config_dir,
                          const char *repos_path)
 {
-   svn_repos_t *repos;
-   apr_hash_t *config;
-   naughtysvn_t *nsvn;
+  svn_repos_t *repos;
+  apr_hash_t *config;
+  naughtysvn_t *nsvn;
 
-   if (n == NULL)
-     nsvn = nsvn_base_init (NULL);
-   else
-     nsvn = (naughtysvn_t*) n;
+  if (n == NULL)
+    nsvn = nsvn_base_init (NULL);
+  else
+    nsvn = (naughtysvn_t*) n;
 
-   if (nsvn == NULL)
-     return EXIT_FAILURE;
+  if (nsvn == NULL)
+    return EXIT_FAILURE;
      
-   apr_hash_t *fs_config = apr_hash_make (nsvn->pool);
+  apr_hash_t *fs_config = apr_hash_make (nsvn->pool);
 
-   apr_hash_set (fs_config, SVN_FS_CONFIG_BDB_TXN_NOSYNC,
-                 APR_HASH_KEY_STRING, (bdb_nosync ? "1" : "0"));
+  apr_hash_set (fs_config, SVN_FS_CONFIG_BDB_TXN_NOSYNC,
+                APR_HASH_KEY_STRING, (bdb_nosync ? "1" : "0"));
 
-   apr_hash_set (fs_config, SVN_FS_CONFIG_BDB_LOG_AUTOREMOVE,
-                 APR_HASH_KEY_STRING, (bdb_autorm ? "1" : "0"));
+  apr_hash_set (fs_config, SVN_FS_CONFIG_BDB_LOG_AUTOREMOVE,
+                APR_HASH_KEY_STRING, (bdb_autorm ? "1" : "0"));
 
-   apr_hash_set (fs_config, SVN_FS_CONFIG_FS_TYPE,
-                 APR_HASH_KEY_STRING, fs_type);
+  apr_hash_set (fs_config, SVN_FS_CONFIG_FS_TYPE,
+                APR_HASH_KEY_STRING, fs_type);
 
-   nsvn->err = svn_config_get_config (&config, nsvn->config_dir,
-                                      nsvn->pool);
-   if (nsvn->err)
-     {
-       if (n == NULL)
-         nsvn = nsvn_base_uninit (nsvn);
-       return EXIT_FAILURE;
-     }
+  nsvn->err = svn_config_get_config (&config, nsvn->config_dir,
+                                     nsvn->pool);
+  if (nsvn->err)
+    {
+      if (n == NULL)
+        nsvn = nsvn_base_uninit (nsvn);
+      return EXIT_FAILURE;
+    }
 
 
-   nsvn->err = svn_repos_create (&repos,
-                                 repos_path,
-                                 NULL, NULL,
-                                 config, fs_config, nsvn->pool);
+  nsvn->err = svn_repos_create (&repos,
+                                repos_path,
+                                NULL, NULL,
+                                config, fs_config, nsvn->pool);
 
-   if (nsvn->err)
-     {
-       if (n == NULL)
-         nsvn = nsvn_base_uninit (nsvn);
-       return EXIT_FAILURE;
-     }
+  if (nsvn->err)
+    {
+      if (n == NULL)
+        nsvn = nsvn_base_uninit (nsvn);
+      return EXIT_FAILURE;
+    }
 
-   /* TODO: Not sure about hooking warning function to fs, like svn_client do. */
+  /* TODO: Not sure about hooking warning function to fs, like svn_client do. */
 
-   if (n == NULL)
-     nsvn = nsvn_base_uninit (nsvn);
+  if (n == NULL)
+    nsvn = nsvn_base_uninit (nsvn);
 
-   return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
 
