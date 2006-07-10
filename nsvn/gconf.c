d1f7097e09cc01fb986a6a66b227134940c79a55
Alexander Thomas <alexander@collab.net>
2006-07-10 12:10:04 +0000

    Added new function.

--- nsvn/gconf.c
+++ nsvn/gconf.c
@@ -18,3 +18,46 @@
 #include <gnome.h>
 
 #include "gconf.h"
+
+
+int
+nsvn_gconf_write_entry (const char *key,
+                        void *value,
+                        GConfClient *gcc)
+{
+  GConfSchema *gcs;
+  GError *gerr;
+  GConfValueType gct;
+
+  if (gcc == NULL || key == NULL)
+    return 0;
+
+  /* Check for the presense of schema, if not return fail. */
+  /* Get key details from schema. */
+  /* Check for the presense of dir, if not create it. */
+  /* if already present update it with new value. */
+  gcs = gconf_client_get_schema (gcc, key, &gerr);
+  if (!gcs) return TRUE;
+
+  gct = gconf_schema_get_type (gcs);
+  switch (gct)
+    {
+      case GCONF_VALUE_STRING:
+        gconf_client_set_string (gcc, key, value, &gerr);
+        break;
+      case GCONF_VALUE_INT:
+        break;
+      case GCONF_VALUE_FLOAT:
+        break;
+      case GCONF_VALUE_BOOL:
+        gconf_client_set_bool (gcc, key, atoi (value), &gerr);
+        break;
+      case GCONF_VALUE_LIST:
+        break;
+      case GCONF_VALUE_PAIR:
+        break;
+      default:
+        break;
+    }
+  return 1;
+}
--- nsvn/gconf.h
+++ nsvn/gconf.h
@@ -11,3 +11,22 @@
 */
 
 #include <gconf/gconf-client.h>
+
+#define NSVN_SVN_CONFIG_DIR_KEY "/apps/naughtysvn/svn_conf_dir"
+#define NSVN_SVN_USERNAME_KEY "/apps/naughtysvn/svn_user_name"
+#define NSVN_SVN_PASSWORD_KEY "/apps/naughtysvn/svn_user_password"
+#define NSVN_CACHE_AUTH_TOKEN_KEY "/apps/naughtysvn/cache_auth_token"
+#define NSVN_PROMPT_USER_KEY "/apps/naughtysvn/prompt_for_user"
+#define NSVN_LOG_TEMPL_DIR_KEY "/apps/naughtysvn/log_templates"
+#define NSVN_LOG_TEMPL_DEFAULT_DIR_KEY "/apps/naughtysvn/log_templates/Default"
+#define NSVN_LOG_TEMPL_DEFAULT_NAME_KEY "/apps/naughtysvn/log_templates/Default/name"
+#define NSVN_LOG_TEMPL_DEFAULT_MSG_KEY "/apps/naughtysvn/log_templates/Default/message"
+#define NSVN_LOG_TEMPL_CUSTOME_DIR_KEY "/apps/naughtysvn/log_templates/%s"
+#define NSVN_LOG_TEMPL_CUSTOME_NAME_KEY "/apps/naughtysvn/log_templates/%s/name"
+#define NSVN_LOG_TEMPL_CUSTOME_MSG_KEY "/apps/naughtysvn/log_templates/%s/message"
+
+
+int
+nsvn_gconf_write_entry (const char *key,
+                        void *value,
+                        GConfClient *gcc);
