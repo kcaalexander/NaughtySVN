9f2284e07b95c94e555d763a504a9f153c112361
Alexander Thomas <alexander@collab.net>
2007-07-02 11:40:49 +0000

    Don't run ./configure script from autogen.sh
    * autogen.sh
      (global): Remove ./configure invocation.
    
    Patch by: Kamesh Jayachandran <kamesh@collab.net>
    (Tweaked by me.)

--- autogen.sh
+++ autogen.sh
@@ -147,13 +147,3 @@ do
     )
   fi
 done
-
-conf_flags="--enable-maintainer-mode"
-
-if test x$NOCONFIGURE = x; then
-  echo Running $srcdir/configure $conf_flags "$@" ...
-  $srcdir/configure $conf_flags "$@" \
-  && echo Now type \`make\' to compile. || exit 1
-else
-  echo Skipping configure process.
-fi
