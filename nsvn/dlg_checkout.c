01cc78c5ad7b6af279872c0b78fd3bec5abe0a97
Alexander Thomas <alexander@collab.net>
2008-01-14 12:14:20 +0000

    Reverting commit  r234. Just loading the checkout_dialog will fail to load the filechooser_dialog. So for the now we load the whole glade file.

--- nsvn/dlg_checkout.c
+++ nsvn/dlg_checkout.c
@@ -471,8 +471,7 @@ nsvn_dlg_checkout (GtkWidget *widget,
   GtkWidget *rel_wid;
 
   /* Error-out if supporting glade file missing in default path. */
-  dlg_gui = glade_xml_new (GLADEDIR "/" DLG_GLADE_FILE,
-                           "checkout_dialog", NULL);
+  dlg_gui = glade_xml_new (GLADEDIR "/" DLG_GLADE_FILE, NULL, NULL);
   if (!dlg_gui)
     {
       g_warning (_("Could not find " GLADEDIR "/" DLG_GLADE_FILE "\n"));
