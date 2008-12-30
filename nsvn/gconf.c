484d0d3a0c3ea092c0b2343d9ecd80fcb541caa4
Alexander Thomas <alexander@collab.net>
2008-12-30 11:34:27 +0000

    Completly implemented reading and writing preferences using gconf.
    * nsvn/gconf.c
       (nsvn_gconf_create_config, nsvn_gconf_clean_config,
        nsvn_gconf_read_config, nsvn_gconf_write_config,
        nsvn_gconf_read_config_entry): New function.
       (nsvn_gconf_write_entry): Renamed ...
       (nsvn_gconf_write_config_entry): ...

--- nsvn/gconf.c
+++ nsvn/gconf.c
@@ -16,17 +16,184 @@
 #endif
 
 #include <gnome.h>
+#include <glib.h>
 
 #include "gconf.h"
+#include "global.h"
 
 
 int
-nsvn_gconf_write_entry (const char *key,
-                        void *value,
+nsvn_gconf_create_config (nsvn_config_t *config)
+{
+  if (config == NULL)
+    return 0;
+
+  config->config_dir = NULL;
+  config->def_username = NULL;
+  config->def_passwd = NULL;
+  config->no_cache_auth = FALSE;
+  config->no_prompt_auth = FALSE;
+  config->no_save_passwd = FALSE;
+
+  return 1;
+}
+
+
+int
+nsvn_gconf_clean_config (nsvn_config_t *config)
+{
+  g_free((void*)config->config_dir);
+  g_free((void*)config->def_username);
+  g_free((void*)config->def_passwd);
+
+  return 1;
+}
+
+
+int
+nsvn_gconf_read_config (nsvn_config_t *config,
                         GConfClient *gcc)
 {
+  if (config == NULL)
+    return 0;
+
+  nsvn_gconf_clean_config(config);
+
+  /* TODO: read config_dir string. */ 
+  config->config_dir = (char*)nsvn_gconf_read_config_entry(
+                          NSVN_SVN_CONFIG_DIR_GCONF_KEY,
+                          NSVN_SVN_CONFIG_DIR_GCONF_SCHEMAS_KEY, gcc);
+                         
+  /* TODO: read def_username string. */ 
+  config->def_username = (char*)nsvn_gconf_read_config_entry(
+                                     NSVN_SVN_USERNAME_GCONF_KEY,
+                                     NSVN_SVN_USERNAME_GCONF_SCHEMAS_KEY, gcc);
+  /* TODO: read def_passwd string. */ 
+  config->def_passwd = (char*)nsvn_gconf_read_config_entry(
+                                     NSVN_SVN_PASSWORD_GCONF_KEY,
+                                     NSVN_SVN_PASSWORD_GCONF_SCHEMAS_KEY, gcc);
+  /* TODO: read no_cache_auth boolean. */
+  config->no_cache_auth = *((gboolean*)nsvn_gconf_read_config_entry(
+                              NSVN_NO_CACHE_AUTH_TOKEN_GCONF_KEY,
+                              NSVN_NO_CACHE_AUTH_TOKEN_GCONF_SCHEMAS_KEY, gcc));
+  /* TODO: read no_prompt_auth boolean. */
+  config->no_prompt_auth = *((gboolean*)nsvn_gconf_read_config_entry(
+                              NSVN_NO_PROMPT_USER_GCONF_KEY,
+                              NSVN_NO_PROMPT_USER_GCONF_SCHEMAS_KEY, gcc));
+  /* TODO: read no_save_passwd boolean. */
+  config->no_save_passwd = *((gboolean*)nsvn_gconf_read_config_entry(
+                              NSVN_NO_STORE_PASSWD_GCONF_KEY,
+                              NSVN_NO_STORE_PASSWD_GCONF_SCHEMAS_KEY, gcc));
+
+  return 1;
+}
+
+
+int
+nsvn_gconf_write_config (nsvn_config_t *config,
+                         GConfClient *gcc)
+{
+  char *dir=NULL;
+  const char *home = NULL;
+
+  if (config == NULL)
+    return 0;
+
+  /* TODO: write config_dir string. */
+  if (config->config_dir == NULL && strcmp(config->config_dir, "") == 0)
+    dir = g_build_path(G_DIR_SEPARATOR_S, ".subversion", NULL);
+  else
+    dir = g_build_path(G_DIR_SEPARATOR_S, config->config_dir, NULL);
+
+  nsvn_gconf_write_config_entry(NSVN_SVN_CONFIG_DIR_GCONF_KEY,
+                                NSVN_SVN_CONFIG_DIR_GCONF_SCHEMAS_KEY,
+                                (void*)dir, gcc);
+  g_free(dir);
+
+  /* TODO: write def_username string. */ 
+  nsvn_gconf_write_config_entry(NSVN_SVN_USERNAME_GCONF_KEY,
+                                NSVN_SVN_USERNAME_GCONF_SCHEMAS_KEY,
+                                (void*)config->def_username, gcc);
+
+  /* TODO: write def_passwd string. */ 
+  nsvn_gconf_write_config_entry(NSVN_SVN_PASSWORD_GCONF_KEY,
+                                NSVN_SVN_PASSWORD_GCONF_SCHEMAS_KEY,
+                                (void*)config->def_passwd, gcc);
+
+  /* TODO: write no_cache_auth boolean. */
+  nsvn_gconf_write_config_entry(NSVN_NO_CACHE_AUTH_TOKEN_GCONF_KEY,
+                                NSVN_NO_CACHE_AUTH_TOKEN_GCONF_SCHEMAS_KEY,
+                                (void*)&(config->no_cache_auth), gcc);
+
+  /* TODO: write no_prompt_auth boolean. */
+  nsvn_gconf_write_config_entry(NSVN_NO_PROMPT_USER_GCONF_KEY,
+                                NSVN_NO_PROMPT_USER_GCONF_SCHEMAS_KEY,
+                                (void*)&(config->no_prompt_auth), gcc);
+
+  /* TODO: write no_save_passwd boolean. */
+  nsvn_gconf_write_config_entry(NSVN_NO_STORE_PASSWD_GCONF_KEY,
+                                NSVN_NO_STORE_PASSWD_GCONF_SCHEMAS_KEY,
+                                (void*)&(config->no_save_passwd), gcc);
+
+  return 1;
+}
+
+
+void*
+nsvn_gconf_read_config_entry (const char *key,
+                              const char *schema,
+                              GConfClient *gcc)
+{
   GConfSchema *gcs;
-  GError *gerr;
+  GConfValueType gct;
+  GError *err=NULL;
+  void *ret=NULL;
+  gboolean *bval = NULL;
+
+  if (gcc == NULL || key == NULL)
+    return NULL;
+
+  /* Check for the presense of schema, if not return fail. */
+  /* Get key details from schema. */
+  /* Check for the presense of dir, if not create it. */
+  /* if already present update it with new value. */
+  gcs = gconf_client_get_schema (gcc, schema, &err);
+  if (!gcs) return NULL;
+
+  gct = gconf_schema_get_type (gcs);
+  switch (gct)
+    {
+      case GCONF_VALUE_STRING:
+        ret = (void*) gconf_client_get_string (gcc, key, &err);
+        break;
+      case GCONF_VALUE_INT:
+        break;
+      case GCONF_VALUE_FLOAT:
+        break;
+      case GCONF_VALUE_BOOL:
+        bval = (gboolean*)g_malloc0(sizeof(gboolean));
+        *bval = gconf_client_get_bool (gcc, key, &err);
+        ret = (void*)bval;
+        break;
+      case GCONF_VALUE_LIST:
+        break;
+      case GCONF_VALUE_PAIR:
+        break;
+      default:
+        break;
+    }
+
+  return ret;
+}
+
+
+int
+nsvn_gconf_write_config_entry (const char *key,
+                               const char *schema,
+                               void *value,
+                               GConfClient *gcc)
+{
+  GConfSchema *gcs;
   GConfValueType gct;
 
   if (gcc == NULL || key == NULL)
@@ -36,21 +203,21 @@ nsvn_gconf_write_entry (const char *key,
   /* Get key details from schema. */
   /* Check for the presense of dir, if not create it. */
   /* if already present update it with new value. */
-  gcs = gconf_client_get_schema (gcc, key, &gerr);
+  gcs = gconf_client_get_schema (gcc, schema, NULL);
   if (!gcs) return TRUE;
 
   gct = gconf_schema_get_type (gcs);
   switch (gct)
     {
       case GCONF_VALUE_STRING:
-        gconf_client_set_string (gcc, key, value, &gerr);
+        gconf_client_set_string (gcc, key, (const char*)value, NULL);
         break;
       case GCONF_VALUE_INT:
         break;
       case GCONF_VALUE_FLOAT:
         break;
       case GCONF_VALUE_BOOL:
-        gconf_client_set_bool (gcc, key, atoi (value), &gerr);
+        gconf_client_set_bool (gcc, key, *((gboolean*)value), NULL);
         break;
       case GCONF_VALUE_LIST:
         break;
--- nsvn/gconf.h
+++ nsvn/gconf.h
@@ -9,24 +9,75 @@
 *
 * ====================================================================
 */
+#ifndef NSVN_GCONF_H
+#define NSVN_GCONF_H
 
 #include <gconf/gconf-client.h>
 
-#define NSVN_SVN_CONFIG_DIR_KEY "/apps/naughtysvn/svn_conf_dir"
-#define NSVN_SVN_USERNAME_KEY "/apps/naughtysvn/svn_user_name"
-#define NSVN_SVN_PASSWORD_KEY "/apps/naughtysvn/svn_user_password"
-#define NSVN_CACHE_AUTH_TOKEN_KEY "/apps/naughtysvn/cache_auth_token"
-#define NSVN_PROMPT_USER_KEY "/apps/naughtysvn/prompt_for_user"
-#define NSVN_LOG_TEMPL_DIR_KEY "/apps/naughtysvn/log_templates"
-#define NSVN_LOG_TEMPL_DEFAULT_DIR_KEY "/apps/naughtysvn/log_templates/Default"
-#define NSVN_LOG_TEMPL_DEFAULT_NAME_KEY "/apps/naughtysvn/log_templates/Default/name"
-#define NSVN_LOG_TEMPL_DEFAULT_MSG_KEY "/apps/naughtysvn/log_templates/Default/message"
-#define NSVN_LOG_TEMPL_CUSTOME_DIR_KEY "/apps/naughtysvn/log_templates/%s"
-#define NSVN_LOG_TEMPL_CUSTOME_NAME_KEY "/apps/naughtysvn/log_templates/%s/name"
-#define NSVN_LOG_TEMPL_CUSTOME_MSG_KEY "/apps/naughtysvn/log_templates/%s/message"
+typedef struct {
+  const char *config_dir;
+  const char *def_username;
+  const char *def_passwd;
+  gboolean no_cache_auth;
+  gboolean no_prompt_auth;
+  gboolean no_save_passwd;
+} nsvn_config_t;
+
+#define NSVN_GCONF_ROOT_KEY "/apps/naughtysvn"
+#define NSVN_GCONF_SCHEMAS_ROOT_KEY "/schemas"NSVN_GCONF_ROOT_KEY
+
+#define NSVN_SVN_CONFIG_DIR_GCONF_KEY NSVN_GCONF_ROOT_KEY"/svn_conf_dir"
+#define NSVN_SVN_USERNAME_GCONF_KEY NSVN_GCONF_ROOT_KEY"/svn_user_name"
+#define NSVN_SVN_PASSWORD_GCONF_KEY NSVN_GCONF_ROOT_KEY"/svn_user_password"
+#define NSVN_NO_CACHE_AUTH_TOKEN_GCONF_KEY NSVN_GCONF_ROOT_KEY"/no_cache_auth_token"
+#define NSVN_NO_PROMPT_USER_GCONF_KEY NSVN_GCONF_ROOT_KEY"/no_prompt_for_user"
+#define NSVN_NO_STORE_PASSWD_GCONF_KEY NSVN_GCONF_ROOT_KEY"/no_store_passwd"
+#define NSVN_LOG_TEMPL_DIR_GCONF_KEY NSVN_GCONF_ROOT_KEY"/log_templates"
+#define NSVN_LOG_TEMPL_DEFAULT_DIR_GCONF_KEY NSVN_GCONF_ROOT_KEY"/log_templates/Default"
+#define NSVN_LOG_TEMPL_DEFAULT_NAME_GCONF_KEY NSVN_GCONF_ROOT_KEY"/log_templates/Default/name"
+#define NSVN_LOG_TEMPL_DEFAULT_MSG_GCONF_KEY NSVN_GCONF_ROOT_KEY"/log_templates/Default/message"
+#define NSVN_LOG_TEMPL_CUSTOME_DIR_GCONF_KEY NSVN_GCONF_ROOT_KEY"/log_templates/%s"
+#define NSVN_LOG_TEMPL_CUSTOME_NAME_GCONF_KEY NSVN_GCONF_ROOT_KEY"/log_templates/%s/name"
+#define NSVN_LOG_TEMPL_CUSTOME_MSG_GCONF_KEY NSVN_GCONF_ROOT_KEY"/log_templates/%s/message"
+
+#define NSVN_SVN_CONFIG_DIR_GCONF_SCHEMAS_KEY NSVN_GCONF_SCHEMAS_ROOT_KEY"/svn_conf_dir"
+#define NSVN_SVN_USERNAME_GCONF_SCHEMAS_KEY NSVN_GCONF_SCHEMAS_ROOT_KEY"/svn_user_name"
+#define NSVN_SVN_PASSWORD_GCONF_SCHEMAS_KEY NSVN_GCONF_SCHEMAS_ROOT_KEY"/svn_user_password"
+#define NSVN_NO_CACHE_AUTH_TOKEN_GCONF_SCHEMAS_KEY NSVN_GCONF_SCHEMAS_ROOT_KEY"/no_cache_auth_token"
+#define NSVN_NO_PROMPT_USER_GCONF_SCHEMAS_KEY NSVN_GCONF_SCHEMAS_ROOT_KEY"/no_prompt_for_user"
+#define NSVN_NO_STORE_PASSWD_GCONF_SCHEMAS_KEY NSVN_GCONF_SCHEMAS_ROOT_KEY"/no_store_passwd"
+#define NSVN_LOG_TEMPL_DIR_GCONF_SCHEMAS_KEY NSVN_GCONF_SCHEMAS_ROOT_KEY"/log_templates"
+#define NSVN_LOG_TEMPL_DEFAULT_DIR_GCONF_SCHEMAS_KEY NSVN_GCONF_SCHEMAS_ROOT_KEY"/log_templates/Default"
+#define NSVN_LOG_TEMPL_DEFAULT_NAME_GCONF_SCHEMAS_KEY NSVN_GCONF_SCHEMAS_ROOT_KEY"/log_templates/Default/name"
+#define NSVN_LOG_TEMPL_DEFAULT_MSG_GCONF_SCHEMAS_KEY NSVN_GCONF_SCHEMAS_ROOT_KEY"/log_templates/Default/message"
+#define NSVN_LOG_TEMPL_CUSTOME_DIR_GCONF_SCHEMAS_KEY NSVN_GCONF_SCHEMAS_ROOT_KEY"/log_templates/%s"
+#define NSVN_LOG_TEMPL_CUSTOME_NAME_GCONF_SCHEMAS_KEY NSVN_GCONF_SCHEMAS_ROOT_KEY"/log_templates/%s/name"
+#define NSVN_LOG_TEMPL_CUSTOME_MSG_GCONF_SCHEMAS_KEY NSVN_GCONF_SCHEMAS_ROOT_KEY"/log_templates/%s/message"
 
 
 int
-nsvn_gconf_write_entry (const char *key,
-                        void *value,
+nsvn_gconf_create_config (nsvn_config_t *config);
+
+int
+nsvn_gconf_clean_config (nsvn_config_t *config);
+
+int
+nsvn_gconf_read_config (nsvn_config_t *config,
                         GConfClient *gcc);
+
+int
+nsvn_gconf_write_config (nsvn_config_t *config,
+                         GConfClient *gcc);
+
+void*
+nsvn_gconf_read_config_entry (const char *key,
+                              const char *schema,
+                              GConfClient *gcc);
+
+int
+nsvn_gconf_write_config_entry (const char *key,
+                               const char *schema,
+                               void *value,
+                               GConfClient *gcc);
+
+#endif /* NSVN_GCONF_H */
